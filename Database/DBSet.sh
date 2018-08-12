5cf600a7b78feab2486027e75d4ec70170963336
root <root@localhost.localdomain>
2018-08-12 10:35:41 +0900

    HWconfig add

--- Database/DBSet.sh
+++ Database/DBSet.sh
@@ -1,4 +1,5 @@
 if ! mysql -u root -ppassword -e 'Alva_Server'; then
-  mysql -u root -ppassword Alva_Server <  BookShelf_scheme.sql
-  mysql -u root -ppassword Alva_Server <  BookData_scheme.sql
+  mysql -u root -ppassword -e 'create database Alva_Server'
+  mysql -u root -ppassword Alva_Server <  /usr/local/lib/SQL_schfiles/BookShelf_scheme.sql
+  mysql -u root -ppassword Alva_Server <  /usr/local/lib/SQL_schfiles/BookData_scheme.sql
 fi
---
+++ HWRecognition/StartProc.sh
@@ -0,0 +1,13 @@
+while :
+do
+  read key
+  if [ "$key" = "q" ]; then
+    echo "breakコマンドを実行します..."
+    break
+  else
+    echo "$keyが入力されました。"
+  fi
+done
+echo "無限ループを抜けました。"
+
+exit 0
