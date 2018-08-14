6fa6f8dbffe2168ae918ba62da1a5aaf11e2ca87
root <root@localhost.localdomain>
2018-08-14 11:01:05 +0900

    commit

--- Database/config/BookData_scheme.sql
+++ Database/config/BookData_scheme.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `D_Bookdata` (
   `barcode` int primary key,
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `book_id` int(10)
+  `book_id` int(10),
   `exist_S` int(1) DEFAULT '0',
-  `book_title` varchar DEFAULT '0',
-  `book_genre` varchar DEFAULT '0',
-  `book_author` varchar DEFAULT '0',
+  `book_title` varchar(100) DEFAULT 'NOT INPUTED',
+  `book_genre` varchar(15) DEFAULT 'NOT INPUTED',
+  `book_author` varchar(15) DEFAULT 'NOT INPUTED',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT fk_book_id
--- Database/init/DBSet.sh
+++ Database/init/DBSet.sh
@@ -1,5 +1,5 @@
-if ! mysql -u root -ppassword -e 'Alva_Server'; then
-  mysql -u root -ppassword -e 'create database Alva_Server'
+if ! mysql -u root -ppassword -e "use Alva_Server"; then
+  mysql -u root -ppassword -e "create database Alva_Server"
   mysql -u root -ppassword Alva_Server <  /usr/local/lib/SQL_schfiles/BookShelf_scheme.sql
   mysql -u root -ppassword Alva_Server <  /usr/local/lib/SQL_schfiles/BookData_scheme.sql
 fi
