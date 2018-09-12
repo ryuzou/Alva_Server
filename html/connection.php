b6fa0e5b799b57f422a6d4a75e2475e572814c23
ryuzou <ryuzot.1112@gmail.com>
2018-09-12 18:30:29 +0900

    update, changed some of the website files.

--- html/connection.php
+++ html/connection.php
@@ -1,16 +1,27 @@
 <?php
-/* Database connection start */
-$servername = "db";
-$username = "root";
-$password = "password";
-$dbname = "Alva_Server";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+Class dbObj
+{
+    /* Database connection start */
+    var $servername = "db";
+    var $username = "root";
+    var $password = "password";
+    var $dbname = "Alva_Server";
+    var $conn;
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+    function getConnstring()
+    {
+        $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
+
+        /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        } else {
+            $this->conn = $con;
+        }
+        return $this->conn;
+    }
 }
 
 ?>
--- html/index.php
+++ html/index.php
@@ -1,75 +1,188 @@
-<?php
-//include connection file 
-include_once("connection.php");
-$sql = "SELECT * FROM `D_BookShelf` limit 1,10 ";
-$queryRecords = mysqli_query($conn, $sql) or die("error to fetch employees data");
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <script type="text/javascript" src="jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css"/>
-    <title>phpflow.com : Simple Example of In-line Editing with HTML5,PHP and MySQL</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Simple Bootgrid example with add,edit and delete using PHP,MySQL and AJAX</title>
+    <link rel="stylesheet" href="dist/bootstrap.min.css" type="text/css" media="all">
+    <link href="dist/jquery.bootgrid.css" rel="stylesheet"/>
+    <script src="dist/jquery-1.11.1.min.js"></script>
+    <script src="dist/bootstrap.min.js"></script>
+    <script src="dist/jquery.bootgrid.min.js"></script>
 </head>
 <body>
-<div class="container" style="padding:50px 250px;">
-    <h1>Simple Example of Inline Editing with HTML5,PHP and MySQL</h1>
-    <div id="msg" class="alert"></div>
-    <table id="employee_grid" class="table table-condensed table-hover table-striped bootgrid-table" width="60%"
-           cellspacing="0">
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>Salary</th>
-            <th>Age</th>
-        </tr>
-        </thead>
-        <tbody id="_editable_table">
-        <?php foreach ($queryRecords as $res) : ?>
-            <tr data-row-id="<?php echo $res['id']; ?>">
-                <td class="editable-col" contenteditable="true" col-index='0'
-                    oldVal="<?php echo $res['employee_name']; ?>"><?php echo $res['employee_name']; ?></td>
-                <td class="editable-col" contenteditable="true" col-index='1'
-                    oldVal="<?php echo $res['employee_salary']; ?>"><?php echo $res['employee_salary']; ?></td>
-                <td class="editable-col" contenteditable="true" col-index='2'
-                    oldVal="<?php echo $res['employee_age']; ?>"><?php echo $res['employee_age']; ?></td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+<div class="container">
+    <div class="">
+        <h1>Simple Bootgrid example with add,edit and delete using PHP,MySQL and AJAX</h1>
+        <div class="col-sm-8">
+            <div class="well clearfix">
+                <div class="pull-right">
+                    <button type="button" class="btn btn-xs btn-primary" id="command-add" data-row-id="0">
+                        <span class="glyphicon glyphicon-plus"></span> Record
+                    </button>
+                </div>
+            </div>
+            <table id="employee_grid" class="table table-condensed table-hover table-striped" width="60%"
+                   cellspacing="0" data-toggle="bootgrid">
+                <thead>
+                <tr>
+                    <th data-column-id="id" data-type="numeric" data-identifier="true">Empid</th>
+                    <th data-column-id="employee_name">Name</th>
+                    <th data-column-id="employee_salary">Salary</th>
+                    <th data-column-id="employee_age">Age</th>
+                    <th data-column-id="commands" data-formatter="commands" data-sortable="false">Commands</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div id="add_model" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Add Employee</h4>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="frm_add">
+                    <input type="hidden" value="add" name="action" id="action">
+                    <div class="form-group">
+                        <label for="name" class="control-label">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="salary" class="control-label">Salary:</label>
+                        <input type="text" class="form-control" id="salary" name="salary"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="salary" class="control-label">Age:</label>
+                        <input type="text" class="form-control" id="age" name="age"/>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" id="btn_add" class="btn btn-primary">Save</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div id="edit_model" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Edit Employee</h4>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="frm_edit">
+                    <input type="hidden" value="edit" name="action" id="action">
+                    <input type="hidden" value="0" name="edit_id" id="edit_id">
+                    <div class="form-group">
+                        <label for="name" class="control-label">Name:</label>
+                        <input type="text" class="form-control" id="edit_name" name="edit_name"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="salary" class="control-label">Salary:</label>
+                        <input type="text" class="form-control" id="edit_salary" name="edit_salary"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="salary" class="control-label">Age:</label>
+                        <input type="text" class="form-control" id="edit_age" name="edit_age"/>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" id="btn_edit" class="btn btn-primary">Save</button>
+            </div>
+            </form>
+        </div>
+    </div>
 </div>
 </body>
 </html>
 <script type="text/javascript">
     $(document).ready(function () {
-        $('td.editable-col').on('focusout', function () {
-            data = {};
-            data['val'] = $(this).text();
-            data['id'] = $(this).parent('tr').attr('data-row-id');
-            data['index'] = $(this).attr('col-index');
-            if ($(this).attr('oldVal') === data['val'])
-                return false;
+        var grid = $("#employee_grid").bootgrid({
+            ajax: true,
+            rowSelect: true,
+            post: function () {
+                /* To accumulate custom parameter with the request object */
+                return {
+                    id: "b0df282a-0d67-40e5-8558-c9e93b7befed"
+                };
+            },
 
-            $.ajax({
+            url: "response.php",
+            formatters: {
+                "commands": function (column, row) {
+                    return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.id + "\"><span class=\"glyphicon glyphicon-edit\"></span></button> " +
+                        "<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\"" + row.id + "\"><span class=\"glyphicon glyphicon-trash\"></span></button>";
+                }
+            }
+        }).on("loaded.rs.jquery.bootgrid", function () {
+            /* Executes after data is loaded and rendered */
+            grid.find(".command-edit").on("click", function (e) {
+                //alert("You pressed edit on row: " + $(this).data("row-id"));
+                var ele = $(this).parent();
+                var g_id = $(this).parent().siblings(':first').html();
+                var g_name = $(this).parent().siblings(':nth-of-type(2)').html();
+                console.log(g_id);
+                console.log(g_name);
 
-                type: "POST",
-                url: "server.php",
-                cache: false,
-                data: data,
-                dataType: "json",
-                success: function (response) {
-                    //$("#loading").hide();
-                    if (!response.error) {
-                        $("#msg").removeClass('alert-danger');
-                        $("#msg").addClass('alert-success').html(response.msg);
-                    } else {
-                        $("#msg").removeClass('alert-success');
-                        $("#msg").addClass('alert-danger').html(response.msg);
-                    }
+                //console.log(grid.data());//
+                $('#edit_model').modal('show');
+                if ($(this).data("row-id") > 0) {
+
+                    // collect the data
+                    $('#edit_id').val(ele.siblings(':first').html()); // in case we're changing the key
+                    $('#edit_name').val(ele.siblings(':nth-of-type(2)').html());
+                    $('#edit_salary').val(ele.siblings(':nth-of-type(3)').html());
+                    $('#edit_age').val(ele.siblings(':nth-of-type(4)').html());
+                } else {
+                    alert('Now row selected! First select row, then click edit button');
+                }
+            }).end().find(".command-delete").on("click", function (e) {
+
+                var conf = confirm('Delete ' + $(this).data("row-id") + ' items?');
+                alert(conf);
+                if (conf) {
+                    $.post('response.php', {id: $(this).data("row-id"), action: 'delete'}
+                        , function () {
+                            // when ajax returns (callback),
+                            $("#employee_grid").bootgrid('reload');
+                        });
+                    //$(this).parent('tr').remove();
+                    //$("#employee_grid").bootgrid('remove', $(this).data("row-id"))
                 }
             });
         });
-    });
 
+        function ajaxAction(action) {
+            data = $("#frm_" + action).serializeArray();
+            $.ajax({
+                type: "POST",
+                url: "response.php",
+                data: data,
+                dataType: "json",
+                success: function (response) {
+                    $('#' + action + '_model').modal('hide');
+                    $("#employee_grid").bootgrid('reload');
+                }
+            });
+        }
+
+        $("#command-add").click(function () {
+            $('#add_model').modal('show');
+        });
+        $("#btn_add").click(function () {
+            ajaxAction('add');
+        });
+        $("#btn_edit").click(function () {
+            ajaxAction('edit');
+        });
+    });
 </script>
