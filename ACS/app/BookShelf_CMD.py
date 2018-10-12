61641b6d066c92e19e1b7f77b23cdd0cd9025c2b
ryuzou <ryuzot.1112@gmail.com>
2018-10-12 16:56:18 +0900

    checkout and add ACS-kotlin-dev

--- ACS/app/BookShelf_CMD.py
+++
@@ -1,9 +0,0 @@
-import flask
-from flask import Flask, jsonify, request
-import json
-
-app = flask.Blueprint('book_cmd', __name__)
-
-@app.route("/cmd/bookshelf", methods=['POST'])
-def RouteBookShelfMiddleCMD():
-    date = json.loads(request.data)
